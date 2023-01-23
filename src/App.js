6a2079bf2266a30404b9edac5f278dee1c568f04
jawher00101~ <jawherj75@gmail.com>
2023-01-23 20:13:46 +0100

    yo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React , { Component } from 'react';
+import Fir from './components/Fir';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
-function App() {
-  return (
+class App extends Component{ 
+  constructor (props){
+    super(props)
+    this.state = {
+      fullName:"jawher rjab",
+      bio: "ay yo im here again", 
+      imgSrc:"monkey.png", 
+      profession: "full stack dev kan 7ab rabi",
+      shows:true,
+      digit: 0
+    }
+  } 
+  componentDidMount = () => {
+    setInterval(() =>{
+      this.setState(prevState =>({
+        digit:prevState.digit + 1
+      }));
+    },
+    1000);
+  }
+  toggle() {
+    this.setState({ shows: !this.state.shows })
+    if (!this.state.shows) {
+      this.setState({digit:0});
+    }
+  }
+  render (){
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      {this.state.shows ? <div> <Fir/>
+      <img src={this.state.imgSrc} alt="im a monkey bro" />
+      <h1>fullName:{this.state.fullName}</h1>
+      <h3>bio:{this.state.bio}</h3>
+      <h4>profession: {this.state.profession}</h4>
+      {/* shows:{this.state.shows} */}
+      </div> :  <div> <h1>GO BACK NOW</h1><img src='https://media4.giphy.com/media/l0HluVRlGyuCOYQhi/giphy.gif' alt='my monkey'/> <p>kindly to reset the component</p> </div>}
+      <button  onClick={() =>this.toggle() }>change state</button>
+      <h5>time interval : {this.state.digit}</h5>
+      
+      </div>
+  )
+  }
 }
 
 export default App;
---
+++ src/components/Fir.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react';
+
+
+class Fir extends Component{ 
+  
+  render (){
+
+  return(
+    <div >
+      <h1>hello from jawher first class componenet</h1>
+      
+      </div>
+  )
+  }
+}
+
+export default Fir
